f8b78c666a2ce0113b285f8d2fb5e8cb0b4a2d44
abdellahsaber07 <abdellah.saber@metave.us>
2023-11-22 23:35:41 +0100

    try 50

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React  seconde  3 4 
+          Learn React  
         </a> 
       </header>
     </div>
