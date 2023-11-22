90e7867ff759786f441bead6f0cbd531a9c7a126
abdellahsaber07 <abdellah.saber@metave.us>
2023-11-23 00:16:42 +0100

    done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Continous deploy ecr to eks 
         </a>
       </header>
     </div>
